521c925d52da21d90828372c36411bfd8913faa4
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-12-01 18:13:28 +0100

    Update day1part2.cs

--- Day1/day1part2.cs
+++ Day1/day1part2.cs
@@ -9,29 +9,33 @@ namespace Day1
 {
     class day1
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            List<int> listOfFrequencies = new List<int>();
-
-
-
             int frequency = 0;
-            var lines = File.ReadLines("input.txt");
-            foreach (var line in lines)
-            { 
-                frequency += Convert.ToInt32(line);
-                
 
-                if (listOfFrequencies.Contains(frequency))
-                    Console.WriteLine(frequency);
+            List<int> lines = File.ReadLines("input.txt").Select(int.Parse).ToList();
 
-                listOfFrequencies.Add(frequency);
+            HashSet<int> listOfFrequencies = new HashSet<int> { frequency };
+
+            while (true)
+            {
+                foreach (var line in lines)
+                {
+
+                    frequency += Convert.ToInt32(line);
+
+
+                    if (listOfFrequencies.Contains(frequency))
+                    {
+                        Console.WriteLine(frequency);
+                        Console.ReadKey();
+                    }
+
+                    listOfFrequencies.Add(frequency);
+                }
             }
 
-            //var hash = new HashSet<int>();
-            //var duplicates = listOfFrequencies.Where(item => !hash.Add(item)).ToList().Distinct().ToList();
-
-            //duplicates.ForEach(Console.WriteLine);
         }
+
     }
 }
