b3f8d23af4a89ac7d43befbdf3f40c3b35cad73b
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-12-02 16:41:25 +0100

    Update day2.cs

--- Day2/day2.cs
+++ Day2/day2.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,19 +14,41 @@ namespace Day2
         {
 
             var lines = File.ReadLines("input.txt");
-            string alphabet = "abcdefghijklmnopqrstuvwxyz"; //TODO Aplbaheb interfavce
 
+            string alphabet = "abcdefghijklmnopqrstuvwxyz"; 
 
+            Boolean has2 = false;
+            Boolean has3 = false;
+
+            int coeff1 = 0;
+            int coeff2 = 0;
+            int checksum = 0;
 
             foreach (var line in lines)
             {
 
                 foreach (char c in alphabet)
                 {
-                    Console.WriteLine(line.Count(x => x == c));
+                    int counter = line.Count(x => x == c);
+
+                    if (counter == 3)
+                        has3 = true;
+
+                    if (counter == 2)
+                        has2 = true;
+
                 }
 
+                if (has3 == true)
+                    coeff1 += 1;
+
+                if (has3 == true)
+                    coeff2 += 1;
+
             }
+            checksum = coeff1 * coeff2;
+            Console.WriteLine(checksum);
+
         }
     }
 }
